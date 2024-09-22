776146a594162afc75c7de9f0978b10e18168b00
Sumshi <sumayasomow@gmail.com>
2024-09-22 13:53:34 +0300

    network scan with nmap

--- Week01/Task01.sh
+++ Week01/Task01.sh
@@ -1,6 +1,6 @@
 # Use Nmap to perform a basic scan of a local network (e.g., `nmap 192.168.1.0/24`).
 ┌──(kali㉿Kali)-[~]
-└─$ nmap 10.5.5.1                                                                                                                                                                                                                          
+└─$ nmap 10.5.5.1                                                                                                                                                                                                                       
 Starting Nmap 7.94 ( https://nmap.org ) at 2024-09-22 08:15 UTC
 Nmap scan report for 10.5.5.1
 Host is up (0.00018s latency).
